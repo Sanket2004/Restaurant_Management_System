64117e98e0d4f2695c047bfef930c205189fa7a8
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2022-10-16 22:29:46 +0530

    menu-driven added

--- resturentManagementSystem.c
+++ resturentManagementSystem.c
@@ -4,5 +4,51 @@
 
 int main()
 {
-    printf("\t\t\t\tRESTAURANT MANAGEMENT SYSTEM\t\t\t\t");
+    int choice;
+    printf("\t\t\t\t--------------------- Restaurant ---------------------\t\t\t\t\n");
+    printf("\t\t\t\tEnter your choice according to the menu given below - \n");
+    printf("\t\t\t\t1 = Book a table.\n");
+    printf("\t\t\t\t2 = Check table availablity.\n");
+    printf("\t\t\t\t3 = Collection of the day.\n");
+    printf("\t\t\t\t4 = Total customers of the day.\n");
+    printf("\t\t\t\t5 = Highest bill of the day.\n");
+    printf("\t\t\t\t6 = Customers details.\n");
+    printf("\t\t\t\t7 = Food menu.\n");
+
+    for (;;)
+    {
+        printf("\t\t\t\tSelect a option:\t");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("\t\t\t\t1 = Book a table.\n");
+            break;
+        case 2:
+            printf("\t\t\t\t2 = Check table availablity.\n");
+            break;
+        case 3:
+            printf("\t\t\t\t3 = Collection of the day.\n");
+            break;
+        case 4:
+            printf("\t\t\t\t4 = Total customers of the day.\n");
+            break;
+        case 5:
+            printf("\t\t\t\t5 = Highest bill of the day.\n");
+            break;
+        case 6:
+            printf("\t\t\t\t6 = Customers details.\n");
+            break;
+        case 7:
+            printf("\t\t\t\t7 = Food menu.\n");
+            break;
+        case 8:
+            exit(0);
+            break;
+
+        default:
+            printf("\t\t\t\tSelect a valid one\n");
+            break;
+        }
+    }
 }
